1b4ace127209d13a9dfb93494132167b818f79e8
estudiantedayana <dayanamh1998@gmail.com>
2025-05-27 10:20:41 -0500

    refactor se cambia el main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String nombre="Diego Alejandro Osorno Cardona";
+        int edad=27;
     }
 }
